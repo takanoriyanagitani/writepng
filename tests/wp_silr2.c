1d1be8e3e36d10a8e93cc8fbc34d49cd0515ecb2
yanagitani <takanori.yanagitani@gmail.com>
2021-01-11 08:14:56 +0000

    idat checked.

---
+++ tests/wp_silr2.c
@@ -0,0 +1,106 @@
+#include "writepng.h"
+
+int on_buf(
+  uint8_t* buf,
+  const uint32_t bufsz,
+  uint8_t* rgba,
+  const uint32_t width
+){
+  switch(wp_set_idat_line_rgba_raw(buf, bufsz, rgba, 1920)){
+    case wp_error_ok: break;
+    default:          return 1;
+  }
+
+  const uint32_t* csize = (const uint32_t*)buf;
+  switch(wp_bswap5(*csize) == (7+1 + 1920*4 + 4)){
+    case true: break;
+    default:   return 1;
+  }
+
+  switch(memcmp(buf+4, "IDAT", 4)){
+    case 0:  break;
+    default: return 1;
+  }
+
+  const uint8_t* cmf = buf+4+4;
+  switch(*cmf){
+    case 0x78: break;
+    default:   return 1;
+  }
+
+  const uint8_t* flg = cmf+1;
+  switch(*flg){
+    case 0x01: break;
+    default:   return 1;
+  }
+
+  const uint16_t* dlen = (const uint16_t*)(flg+1);
+  switch(*dlen){
+    case 1 + 4*1920: break;
+    default:         return 1;
+  }
+
+  const uint16_t* nlen = dlen+1;
+  switch(*dlen+*nlen){
+    case 0xffff: break;
+    default:     return 1;
+  }
+
+  const uint8_t* pixdata = (const uint8_t*)(nlen+1);
+  switch(pixdata[0]){
+    case 0:  break;
+    default: return 1;
+  }
+  for(int i=0; i<4*width; i++){
+    switch(pixdata[1+i]){
+      case 0:  break;
+      default: return 1;
+    }
+  }
+
+  const uint32_t* adler = (const uint32_t*)(pixdata + 1 + 4*1920);
+  switch(wp_bswap5(*adler)){
+    case 0x1e010001: break;
+    default:         return 1;
+  }
+
+  const uint32_t* crc = adler+1;
+  switch(wp_bswap5(*crc)){
+    case 0x78ba7068: return 0;
+    default:         break;
+  }
+  fprintf(stderr, "got: %x\n", wp_bswap5(*crc));
+  return 1;
+}
+
+int fullhd(){
+  switch(wp_set_idat_line_rgba_raw(NULL, 0, NULL, 1920)){
+    case wp_error_mem_short: break;
+    default:                 return 1;
+  }
+  const uint32_t bufsz = 0
+    +4+4
+    +2+1+8
+    +1+1920*4
+    +4
+    +4
+  ;
+  uint8_t* buf  = calloc(bufsz*2, 1);
+  uint8_t* rgba = buf+bufsz;
+
+  int e = on_buf(buf, bufsz, rgba, 1920);
+  free(buf);
+  return wp_error_ok == e ? 0 : 1;
+}
+
+int main(int argc, char** argv){
+  switch(argc){
+    case 2:  break;
+    default: return 1;
+  }
+  const char* type = argv[1];
+  switch(type[0]){
+    case 'f': return fullhd();
+    default:  return 1;
+  }
+}
--- writepng.h
+++ writepng.h
@@ -9,17 +9,19 @@
 
 #include <zlib.h>
 
+uint32_t wp_bswap5(const uint32_t x){ return __builtin_bswap32(x); }
+
 typedef enum {
   wp_error_ok        = 0,
   wp_error_mem_short = 1,
 } wp_error;
 
-uint32_t wp_adler32(const uint8_t* data, uint32_t len){
+uint32_t wp_adler32(const void* data, uint32_t len){
   uint32_t a = adler32(0L, Z_NULL, 0);
   return adler32(a, data, len);
 }
 
-uint32_t wp_crc32(const uint8_t* data, uint32_t len){
+uint32_t wp_crc32(const void* data, uint32_t len){
   uint32_t c = crc32(0L, Z_NULL, 0);
   return crc32(c, data, len);
 }
@@ -30,13 +32,11 @@ int wp_set_header(uint8_t* buf, const uint32_t len){
     default:   return wp_error_mem_short;
   }
   buf[0]=0x89;
-  snprintf(buf+1, 6, "PNG\r\n");
+  snprintf((char*)buf+1, 6, "PNG\r\n");
   buf[6]=0x1a;
   buf[7]=0x0a;
   return wp_error_ok;
 }
-
-uint32_t wp_bswap5(const uint32_t x){ return __builtin_bswap32(x); }
 
 int wp_set_ihdr_rgba_raw(
   uint8_t* buf,
@@ -77,6 +77,69 @@ int wp_set_ihdr_rgba_raw(
 
   uint32_t* chk = (uint32_t*)(interlace+1);
   *chk = wp_bswap5(wp_crc32(buf+4, 4+13));
+
+  return wp_error_ok;
+}
+
+int wp_set_idat_line_rgba_raw(
+  uint8_t* buf,
+  const uint32_t len,
+  const uint8_t* data,
+  const uint32_t width
+){
+  const uint32_t org_size = 0
+    + 1       // filter type
+    + 4*width // 4*rgba
+  ;
+  const uint32_t def_required = 0
+    + 2        // CMF,FLG
+    + 1        // BFINAL,BTYPE
+    + 4        // len,nlen
+    + org_size
+    + 4        // adler32
+  ;
+  const uint32_t len_required = 0
+    + 4             // chunk size
+    + 4             // IDAT
+    + def_required
+    + 4             // crc32(png)
+  ;
+  switch(len_required <= len){
+    case true: break;
+    default:   return wp_error_mem_short;
+  }
+
+  uint32_t* csize = (uint32_t*)buf;
+  *csize = wp_bswap5(def_required);
+
+  memcpy(buf+4, "IDAT", 4);
+
+  uint8_t* cmf = buf+4+4;
+  *cmf = 0x78; // CM=8, CINFO=7(deflate, window size=32k)
+
+  uint8_t* flg = cmf+1;
+  *flg = 0x01; // no dict, fastest compression
+
+  uint16_t* dlen = (uint16_t*)(flg+1);
+  *dlen = org_size;
+
+  uint16_t* nlen = dlen+1;
+  *nlen = *dlen ^ 0xffff;
+
+  uint8_t* pixdata = (uint8_t*)(nlen+1);
+  *pixdata = 0x00; // no filter
+  memcpy(pixdata+1, data, 4*width);
+
+  uint32_t* adler = (uint32_t*)(pixdata + 1 + 4*width);
+  *adler = wp_bswap5(wp_adler32(pixdata, 1 + 4*width));
+
+  uint32_t* c = adler+1;
+  *c = wp_bswap5(wp_crc32(
+    buf+4,         // IDAT
+    0
+    + 4            // IDAT
+    + def_required
+  ));
 
   return wp_error_ok;
 }
